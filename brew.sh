af0dabae7662cd508018f4ca14a49a1cb9293206
Max Barrass <max.barrass@gmail.com>
2019-03-13 01:14:50 +1100

    update imagemagick

--- brew.sh
+++ brew.sh
@@ -148,7 +148,7 @@ brew install git-flow
 brew install git-extras
 brew install hub
 brew install librsvg
-brew install imagemagick --with-webp --with-librsvg
+brew install imagemagick
 brew install lua
 brew install lynx
 brew install p7zip

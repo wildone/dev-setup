8a5ac0c61b351b7789c1a9877328c0e56bdeb857
Max Barrass <max.barrass@gmail.com>
2019-03-12 23:02:16 +1100

    remove apps from kill command

--- osx.sh
+++ osx.sh
@@ -794,8 +794,8 @@ cp -r init/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ 
 
 for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
     "Dock" "Finder" "Google Chrome" "Google Chrome Canary" "Mail" "Messages" \
-    "Opera" "Safari" "SizeUp" "Spectacle" "SystemUIServer" \
-    "Transmission" "Twitter" "iCal"; do
+    "Opera" "Safari" "SystemUIServer" \
+    "iCal"; do
     killall "${app}" > /dev/null 2>&1
 done
 echo "Done. Note that some of these changes require a logout/restart of your OS to take effect.  At a minimum, be sure to restart your Terminal."

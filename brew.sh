ea8d316ebeb3ec74f9e0129fc6055d080d2c8a33
Max Barrass <max.barrass@gmail.com>
2019-03-13 00:15:12 +1100

    remove vim cleanup dupes

--- brew.sh
+++ brew.sh
@@ -59,10 +59,10 @@ brew install python
 brew install python3
 
 # Install more recent versions of some OS X tools.
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
+#brew install vim
+brew install grep
+brew install openssh
+brew install screen
 #brew install homebrew/php/php55 --with-gmp
 
 # Install font tools.

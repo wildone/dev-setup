c82095e2bcb379fa33b7d5215c14cbe67c6b15d5
Max Barrass <max.barrass@gmail.com>
2019-03-13 00:08:23 +1100

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -21,6 +21,8 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade
 
+brew cask install homebrew/cask-versions/java8
+
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
